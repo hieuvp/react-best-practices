48c761f3fc9aa33205ebda4ce5fea51256123e3e
Hieu Van Phu <hieu.van@pyramid-consulting.com>
2016-12-28 21:14:23 +0700

    Flow Typed Config

--- src/scene/home/HomeContainer.js
+++ src/scene/home/HomeContainer.js
@@ -2,3 +2,32 @@
 /**
  * Created by HieuVP on 12/28/16.
  */
+import React from 'react';
+import Radium from 'radium';
+import BaseContainer from '../../application/BaseContainer';
+
+class HomeContainer extends BaseContainer {
+
+  static get TAG_NAME() {
+    return HomeContainer.name;
+  }
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <h2>Welcome to React</h2>
+      </div>
+    );
+  }
+
+}
+
+const styles = {
+  container: {},
+};
+
+export default Radium(HomeContainer);
