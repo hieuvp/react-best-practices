a83f1ef11d84ccb98504d5aeafe0387360affdc1
Hieu Van Phu <hieu.van@pyramid-consulting.com>
2017-01-02 22:57:37 +0700

    gulp wait

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import gulp from 'gulp';
+import wait from 'gulp-wait';
 import shell from 'gulp-shell';
 import copy from 'gulp-copy';
 import runSequence from 'run-sequence';
@@ -17,16 +18,19 @@ const definitelyTypedSource = [
 
 gulp.task('install-node-modules', () => gulp.src(path.parent)
   .pipe(shell(`rm -rf ${path.nodeModules}`))
+  .pipe(wait(1000))
   .pipe(shell('npm install'))
 );
 
 gulp.task('install-flow-typed', () => gulp.src(path.parent)
   .pipe(shell(`rm -rf ${path.flowTyped}`))
+  .pipe(wait(1000))
   .pipe(shell('flow-typed install'))
 );
 
 gulp.task('install-definitely-typed', () => gulp.src(definitelyTypedSource)
   .pipe(shell(`rm -rf ${path.definitelyTyped}`))
+  .pipe(wait(1000))
   .pipe(copy(path.definitelyTyped, {prefix: 10}))
 );
 
