4b9329df42aaaad40701ed9b112f4641de9744c0
Hieu Van Phu <hieu.van@pyramid-consulting.com>
2017-01-02 16:20:44 +0700

    Clean Install Task

---
+++ build-system/StringGenerator.js
@@ -0,0 +1,9 @@
+/**
+ * Created by HieuVP on 1/2/17.
+ */
+
+class StringGenerator {
+
+}
+
+export default new StringGenerator();
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,14 @@
 import 'babel-polyfill';
 import gulp from 'gulp';
-import dependencyManager from './build-system/DependencyManager';
+import shell from 'gulp-shell';
 
-gulp.task('clean-install', () => {
-  dependencyManager.clean();
-  dependencyManager.install();
-});
+const path = {
+  parent: './',
+};
+
+gulp.task('clean-install', () => gulp.src(path.parent)
+  .pipe(shell('rm -rf node_modules'))
+  .pipe(shell('rm -rf flow-typed'))
+  .pipe(shell('npm install'))
+  .pipe(shell('flow-typed install'))
+);
