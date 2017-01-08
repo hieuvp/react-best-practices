b935d1e0601e3aaf1c2a82ec79785b052da1caea
Hieu Van Phu <hieu.van@pyramid-consulting.com>
2017-01-08 20:07:14 +0700

    Firebase Deploy Task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,6 +8,7 @@ const path = {
   parent: '/',
   nodeModules: 'node_modules',
   flowTyped: 'flow-typed',
+  build: 'build',
 };
 
 gulp.task('install-node-modules', () => gulp.src(path.parent)
@@ -25,3 +26,13 @@ gulp.task('install-flow-typed', () => gulp.src(path.parent)
 gulp.task('clean-install', ['install-node-modules'], (callback) => {
   runSequence('install-flow-typed', callback);
 });
+
+gulp.task('deploy-to-firebase', () => gulp.src(path.parent)
+  .pipe(shell(`rm -rf ${path.build}`))
+  .pipe(wait(1000))
+  .pipe(shell('npm run build'))
+  .pipe(wait(1000))
+  .pipe(shell('firebase deploy'))
+  .pipe(wait(1000))
+  .pipe(shell(`rm -rf ${path.build}`))
+);
