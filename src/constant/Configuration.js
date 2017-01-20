00c940779142f450ce0b88de57f280d4492e0a9a
Hieu Van Phu <hieu.van@pyramid-consulting.com>
2017-01-20 13:40:47 +0700

    Refactor

--- src/constant/Configuration.js
+++ src/constant/Configuration.js
@@ -31,27 +31,27 @@ const firebase = (() => {
   switch (profile) {
     case Environment.DEVELOPMENT:
       return {
-        apiKey: "AIzaSyD2IF-oUGlDrQQdqG4j1fOHUYX2FMlikuQ",
-        authDomain: "awesome-chat-development.firebaseapp.com",
-        databaseURL: "https://awesome-chat-development.firebaseio.com",
-        storageBucket: "awesome-chat-development.appspot.com",
-        messagingSenderId: "736326604442"
+        apiKey: 'AIzaSyD2IF-oUGlDrQQdqG4j1fOHUYX2FMlikuQ',
+        authDomain: 'awesome-chat-development.firebaseapp.com',
+        databaseURL: 'https://awesome-chat-development.firebaseio.com',
+        storageBucket: 'awesome-chat-development.appspot.com',
+        messagingSenderId: '736326604442',
       };
     case Environment.STAGING:
       return {
-        apiKey: "AIzaSyBE3ZIwEAqXXLHqc92rAz7ffp8M3qItC8U",
-        authDomain: "awesome-chat-staging.firebaseapp.com",
-        databaseURL: "https://awesome-chat-staging.firebaseio.com",
-        storageBucket: "awesome-chat-staging.appspot.com",
-        messagingSenderId: "100781343696"
+        apiKey: 'AIzaSyBE3ZIwEAqXXLHqc92rAz7ffp8M3qItC8U',
+        authDomain: 'awesome-chat-staging.firebaseapp.com',
+        databaseURL: 'https://awesome-chat-staging.firebaseio.com',
+        storageBucket: 'awesome-chat-staging.appspot.com',
+        messagingSenderId: '100781343696',
       };
     case Environment.PRODUCTION:
       return {
-        apiKey: "AIzaSyAX8mJVDX63c9N178RRXzGFsYW9tqIJHss",
-        authDomain: "awesome-chat-production.firebaseapp.com",
-        databaseURL: "https://awesome-chat-production.firebaseio.com",
-        storageBucket: "awesome-chat-production.appspot.com",
-        messagingSenderId: "967250077961"
+        apiKey: 'AIzaSyAX8mJVDX63c9N178RRXzGFsYW9tqIJHss',
+        authDomain: 'awesome-chat-production.firebaseapp.com',
+        databaseURL: 'https://awesome-chat-production.firebaseio.com',
+        storageBucket: 'awesome-chat-production.appspot.com',
+        messagingSenderId: '967250077961',
       };
     default:
       return undefined;
